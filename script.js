7d2fd979820b9bbc73fdbe67f8720c6648bd98e7
Danjor225 <179250247+Danjor225@users.noreply.github.com>
2025-02-27 17:20:52 +0000

    Create gameboard factory with create grid function
    This allows variable to be assigned to create grid
    function.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-const test = document.createElement('div');
 
-document.body.appendChild(test)
-test.textContent = "testing"
+function Gameboard () {
+
+    const row = 3;
+    const column = 3;
+
+    function createGrid() {
+        const gridArray = []
+        for (let x = 0; x < row; x ++){
+            gridArray[x] = []
+            for(let y = 0; y <column; y++){
+                gridArray[x].push(y);
+            }
+        }
+        return gridArray
+    }
+
+    return {createGrid}
+}
+
+const gameBoard = Gameboard()
+
+let grid = gameBoard.createGrid()
+
+console.log(grid)
